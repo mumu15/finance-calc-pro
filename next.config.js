3b3f6ea2f28ce7c6ee26756e2ef5723e58730dde
Munir <munafr@gmail.com>
2026-03-20 22:16:22 +0100

    Add 9 redirects to next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  async redirects(){return[{source:"/investment-calculator",destination:"/investment-return-calculator",permanent:true},{source:"/personal-loan",destination:"/personal-loan-calculator",permanent:true},{source:"/income-tax-calculator",destination:"/tax-calculator",permanent:true},{source:"/inventory-turnover-calculator",destination:"/accounts-receivable-calculator",permanent:true},{source:"/revenue-growth-calculator",destination:"/roi-calculator",permanent:true},{source:"/gas-mileage-calculator",destination:"/fuel-cost-calculator",permanent:true},{source:"/pricing-calculator",destination:"/markup-calculator",permanent:true},{source:"/road-trip-cost-calculator",destination:"/fuel-cost-calculator",permanent:true},{source:"/credit-score-calculator",destination:"/credit-utilization-calculator",permanent:true}]},
   async headers() {
     return [
       {
